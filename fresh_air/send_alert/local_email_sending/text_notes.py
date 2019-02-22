eabe9f5601eef89a73dfe4609791c31b289a040f
Megan Thomas <thomasmt@mail.gvsu.edu>
2019-02-21 19:40:53 -0500

    working text notifications
    I have a script that sends texts to a number from our email. will likely have to change tho

--- fresh_air/send_alert/local_email_sending/text_notes.py
+++ fresh_air/send_alert/local_email_sending/text_notes.py
@@ -1,9 +1,14 @@
+## most simple way i could find to send texts through email will probably change 
+
+import smtplib
 import boto3
 import json
 import decimal
 from boto3.dynamodb.conditions import Key, Attr
 
 ##TO DO: move this info to a credentials file 
+MY_ADDRESS = 'teambreatheoffreshair@gmail.com'
+PASSWORD = 'CIS467BFA'
 ACCESS_KEY = 'AKIAIJ55NBMNXJBAX2MA'
 SECRET_KEY = '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'
 
@@ -20,7 +25,6 @@ class DecimalEncoder(json.JSONEncoder):
 
 # pulls contact info from the db 
 def get_contacts_phone():
-    contacts_names = []
     contacts_phone = []
     dynamodb = boto3.resource('dynamodb',
                               aws_access_key_id=ACCESS_KEY,
@@ -35,13 +39,38 @@ def get_contacts_phone():
     for i in response["Items"]:
         temp = str(json.dumps(i["phone"], cls=DecimalEncoder))
         temp = temp.replace("\"", "")
-        contacts.append(temp)
-        temp = str(json.dumps(i["name"], cls=DecimalEncoder))
-        temp = temp.replace("\"", "")
-        contacts_names.append(temp)
+        contacts_phone.append(temp)
 
-    return contacts_names, contacts_phone
+    return contacts_phone
 
-def send_texts():
-	
-	names, numbers = get_contacts_phone()
+def send_texts(message):
+    
+    carriers = {
+    'att':    '@mms.att.net',
+    'tmobile':' @tmomail.net',
+    'verizon':  '@vtext.com',
+    'sprint':   '@page.nextel.com'
+    }
+
+    numbers = get_contacts_phone()
+    
+    #temp with my number for testing 
+    to_number = '5862601818{}'.format(carriers['att'])
+    auth = (MY_ADDRESS, PASSWORD)
+
+    # Establish a secure session with gmail's outgoing SMTP server using your gmail account
+    #SMTP server
+    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    s.starttls()
+    s.login(auth[0], auth[1])
+
+    for i in numbers: 
+        text = "Megan "
+        text += message
+        s.sendmail( auth[0], to_number, text)
+
+def main():
+    send_texts("AIR QUALITY ALERT")
+    
+if __name__ == '__main__':
+    main()
