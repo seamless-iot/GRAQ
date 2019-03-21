457412b94526a11296f73dfdae5faed05302d3b4
tgroelsma <32466048+tgroelsma@users.noreply.github.com>
2019-03-21 13:10:41 -0400

    Added preliminary csv file maker
    New send_csv file in data_manage, has two functions to call (one for ost table and one for simms table) that will make a csv file in the specified output path

---
+++ fresh_air/data_manage/send_csv.py
@@ -0,0 +1,76 @@
+import boto3
+import json
+import decimal
+import datetime
+import csv
+from boto3.dynamodb.conditions import Key, Attr
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+def accessDatabase(dbName):
+    ACCESS_KEY = 'AKIAIJ55NBMNXJBAX2MA'
+    SECRET_KEY = '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'
+
+
+    dynamodb = boto3.resource('dynamodb',
+                              aws_access_key_id=ACCESS_KEY,
+                              aws_secret_access_key=SECRET_KEY,
+                              region_name='us-west-2'
+                             )
+
+    table = dynamodb.Table(dbName)
+    response = table.scan()
+    returnData = response['Items']
+    return returnData
+
+def ostFile(outputPath):
+    ostData = accessDatabase('ost_data')
+    currentDT = datetime.datetime.now()
+    filename = "ost_" + currentDT.strftime("%Y-%m-%dT%H-%M-%S") + ".csv"
+    with open(outputPath + filename, 'w+', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        colValues = ostData[0].keys()
+        writeRow = ['dev_id', 'time']
+        for i in colValues:
+            if i != 'dev_id' and i != 'time':
+                writeRow.append(i)
+        filewriter.writerow(writeRow)
+        for i in ostData:
+            writeRow = []
+            writeRow.append(i['dev_id'])
+            writeRow.append(i['time'])
+            for key in i:
+                if key != 'dev_id' and key != 'time':
+                    writeRow.append(i[key])
+            filewriter.writerow(writeRow)
+
+def simmsFile(outputPath):
+    simmsData = accessDatabase('simms_data')
+    currentDT = datetime.datetime.now()
+    filename = "simms_" + currentDT.strftime("%Y-%m-%dT%H-%M-%S") + ".csv"
+    with open(outputPath + filename, 'w+', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        colValues = simmsData[0].keys()
+        writeRow = ['dev_id', 'time']
+        for i in colValues:
+            if i != 'dev_id' and i != 'time':
+                writeRow.append(i)
+        filewriter.writerow(writeRow)
+        for i in simmsData:
+            writeRow = []
+            writeRow.append(i['dev_id'])
+            writeRow.append(i['time'])
+            for key in i:
+                if key != 'dev_id' and key != 'time':
+                    writeRow.append(i[key])
+            filewriter.writerow(writeRow)
+
+ostFile('C:/Users/etern/Documents/GitHub/freshair/fresh_air/data_manage/')
+simmsFile('C:/Users/etern/Documents/GitHub/freshair/fresh_air/data_manage/')
