ad5c0bc01f16deda01dc0f42ed56ad2674315226
Javier Ramirez <ramijavi@mail.gvsu.edu>
2019-04-19 19:01:58 -0400

    Added averaging algorithm to neighborhood maps
    Also cleaned the code a little bit more and wrote further documentation

--- fresh_air/data_visualize/views.py
+++ fresh_air/data_visualize/views.py
@@ -20,34 +20,31 @@ def test(request):
     import dataPull
     import json
     from matplotlib import path
+    import math
 
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     mapbox_access_token = "pk.eyJ1IjoicmFtaWphdmkiLCJhIjoiY2pyemJ5bm56MTdhMzRhbXRscjA0djd0dSJ9.TDjuO5EJnwFcz7hZCEXXwA"
 
-    # Initiating and running dataPull script
-    # This script is used to pull data from the DynamoDB
+    # Initiating and running dataPull script. This script is used to pull data from the DynamoDB
     graphData = dataPull.graphDataGetter()
     graphData.run()
 
-    # getLocationData() returns the location, device ID, device type
-    # and AQI for all sensors on the DB
+    # getLocationData() returns the location, device ID, device type and AQI for all sensors on the DB
     locationData = graphData.getLocationData()
 
-    # For debugging purposes
-    # print(locationData)
-
+    # Instantiating variables
     lat = ''
     lon = ''
     sensorList = []
     neighborhoods = []
     buff = ''
+    listForMap = []
+    AQIList = []
+    neighborhoodsToModify = []
 
-    # This nested for loop gets the data from getLocationData()
-    # and stores the relevant variables on the sensorList dictionary
-    # list, which will later be used.
-    # getLocationData() returns the GPS coordinates as a string, so
-    # they must be separated into latitude and longitude and then
-    # stored as floats
+    # This nested for loop gets the data from getLocationData() and stores the relevant variables on the sensorList
+    # dictionary list, which will later be used. getLocationData() returns the GPS coordinates as a string, so
+    # they must be separated into latitude and longitude and then stored as floats on different variables
     for i in locationData:
         index = 0
         for a in i['device_gps_location']:
@@ -68,34 +65,27 @@ def test(request):
         lat = ''
         lon = ''
 
-    # Open the GeoJson file containing the different neighborhoods
-    # of Grand Rapids
+    # Open the GeoJson file containing the different neighborhoods of Grand Rapids
     with open('City_of_Grand_Rapids_Neighborhood_Areas.geojson') as f:
         geoFile = json.load(f)
 
-    # From this file we only need the name of the neighborhoods, in order
-    # to be able to identify them, and the coordinates of the polygons
-    # that make up the neighborhood shapes. These variables will be saved
-    # to the neighborhoods dictionary list.
+    # From this file we only need the name of the neighborhoods, in order to be able to identify them, and the coordinates
+    # of the polygons that make up the neighborhood shapes. These variables will be saved to the neighborhoods dictionary
+    # list
     for i in geoFile['features']:
         neighborhoods.append(dict(
             name=i['properties']['NEBRH'],
             coordinates=i['geometry']['coordinates']
         ))
 
-    listForMap = []
-
     # Loop through every entry of the neighborhoods dictionary list
     for i in neighborhoods:
-
         # Create two copies of the neighborhood coordinates
         coordinates = i['coordinates']
         coordinates2 = i['coordinates']
 
-        # For every neighborhood in the neighborhoods dict list
-        # we create a temporary GeoJson object (a GeoJson object
-        # is the same as a Json object, but specific for mapping
-        # points, shapes, etc.)
+        # For every neighborhood in the neighborhoods dict list we create a temporary GeoJson object
+        # A GeoJson object is the same as a Json object, but specific for mapping points, shapes, etc.
         tempJson = {
           "type": "FeatureCollection",
           "features": [
@@ -112,10 +102,8 @@ def test(request):
           ]
         }
 
-        # In the second copy the neighborhood coordinates we will
-        # change the order of the latitude and longitude. This is
-        # because the matPlotLib, which is used below, uses the
-        # format (latitude, longitude) while GeoJson uses the
+        # In the second copy the neighborhood coordinates we will change the order of the latitude and longitude. This is
+        # because the matPlotLib, which is used below, uses the format (latitude, longitude) while GeoJson uses the
         # format (longitude, latitude).
         latHolder = coordinates2[0]
         coordinates2[0] = coordinates2[1]
@@ -125,30 +113,20 @@ def test(request):
         sensorNum = 0
 
         for a in sensorList:
-            # For every sensor in sensorList a point will be created
+            # For every sensor in sensorList a temp point is created
             tempPoint = a['coordinates']
             # Check if the point is contained in the neighborhood
             if tempPath.contains_point(tempPoint):
-                # For debugging purposes
-                # print("contains point")
-                # If the sensor is in the neighborhood, then sum its
-                # AQI to AQICount and increase the sensor number by 1
+                # If the sensor is in the neighborhood, then sum its AQI to AQICount and increase the sensor number by 1
                 AQICount += a['AQI']
                 sensorNum += 1
 
-        # For debugging purposes
-        # print(AQICount)
-        # print(sensorNum)
-
         # Check if there is at least 1 sensor the neighborhood
         if sensorNum != 0:
-            # Compute the AQI average from the reading from
-            # the different sensors
+            # Compute the AQI average from the reading from the different sensors
             AQIAvg = AQICount/sensorNum
-            # For debugging purposes
-            # print(AQIAvg)
-            # Assign a color to the neighborhood depending on its
-            # AQI measurements. This color assignment corresponds
+            AQIList.append(AQIAvg)
+            # Assign a color to the neighborhood depending on its AQI measurements. This color assignment corresponds
             # to the map legend
             if AQIAvg <= 16:
                 tempColor = 'rgba(25, 191, 0, 1)'
@@ -189,29 +167,22 @@ def test(request):
         # If neighborhood has no data, assign color grey
         else:
             tempColor = 'rgba(67, 67, 67, 0.4)'
+            AQIList.append(0)
 
         AQICount = 0
         sensorNum = 0
 
-        # For debugging purposes
-        # print(tempColor)
-        # Append a dictionary to the listForMap list. This dict
-        # includes the temporary GeoJson object created from the
-        # neighborhood, the color assigned, and other properties
-        # that are common to all neighborhoods in the list
+        # Append a dictionary to the listForMap list. This dict includes the temporary GeoJson object created from the
+        # neighborhood, the color assigned, and other properties that are common to all neighborhoods in the list
         listForMap.append(dict(
             opacity=0.8,
             sourcetype='geojson',
             source=tempJson,
             type='fill',
-            color=tempColor,
-            fill={
-                'outlinecolor': '#000000'
-            }
+            color=tempColor
         ))
-        # For every neighborhood, another additional dictionary will
-        # be appended for drawing the borders of each neighborhood.
-        # This is why type='line'
+        # For every neighborhood, another additional dictionary will be appended for drawing the borders of each
+        # neighborhood. This is why type='line'
         listForMap.append(dict(
             opacity=0.8,
             sourcetype='geojson',
@@ -223,7 +194,91 @@ def test(request):
             }
         ))
 
-    app = DjangoDash('Neighborhood', external_stylesheets=external_stylesheets)
+    # This is the AQI Averaging Algorithm. It will check the neighborhoods that have no AQI data and will approximate an
+    # average AQI based on the data of its neighbors. Therefore, only neighborhoods that are contiguous to other
+    # neighborhoods with data will be able to get an approximate AQI.
+    # This can be a very CPU-intensive algorithm and it may slow down the loading of the maps quite a bit for certain
+    # users with a slow internet connection or a slow CPU.
+    #
+    # The principle of this algorithm is the following: For every neighborhood with no data, it will check if there are any
+    # neighborhoods with data that are adjacent to it. It will do this by checking the distance of all the points of the
+    # two neighborhoods being checked. If the points are close enough, then it considers the neighborhoods to be adjacent
+    #
+    # Loop through all neighborhoods in the listForMap list
+    for nonFilledNeighborhood in listForMap:
+        # If the index is even and has no AQI data
+        if listForMap.index(nonFilledNeighborhood) % 2 == 0 and nonFilledNeighborhood['color'] == 'rgba(67, 67, 67, 0.4)':
+            # Reset AQI approximation and count variables to 0
+            AQIApprox = 0
+            approxCt = 0
+            # Loop through all neighborhoods in the listForMap list
+            for filledNeighborhood in listForMap:
+                # If the index is even and has AQI data
+                if listForMap.index(filledNeighborhood) % 2 == 0 and filledNeighborhood['color'] != 'rgba(67, 67, 67, 0.4)':
+                    # Assume the neighborhoods are not adjacent
+                    isAdjacent = False
+                    # Loop through all the points of the neighborhood maps
+                    for pt1 in filledNeighborhood['source']['features'][0]['geometry']['coordinates'][0]:
+                        for pt2 in nonFilledNeighborhood['source']['features'][0]['geometry']['coordinates'][0]:
+                            # Calculate the distance between the points
+                            if math.sqrt(((pt1[1] - pt2[1])**2)+((pt1[0] - pt2[0])**2)) <= 0.0001:
+                                # If the distance is small enough, then the neighborhoods are adjacent
+                                isAdjacent = True
+                                # If it's adjacent, then increase approx count by 1 and break out of the inner loop
+                                approxCt += 1
+                                break
+                        if isAdjacent:
+                            # Break out of the outer loop
+                            break
+                    if isAdjacent:
+                        # If the neighborhood is adjacent, then add it's AQI to AQIApprox
+                        AQIApprox += AQIList[int((listForMap.index(filledNeighborhood))/2)]
+            if approxCt != 0:
+                # Append dictionary with index and approximate AQI of neighborhood whose AQI we've estimated
+                neighborhoodsToModify.append(dict(
+                    index=listForMap.index(nonFilledNeighborhood),
+                    AQI=AQIApprox/approxCt
+                ))
+    # Loop through the neighborhoodsToModify list and change the color of the neighborhood according to its AQI
+    for x in neighborhoodsToModify:
+        if x['AQI'] <= 16:
+            listForMap[x['index']]['color'] = 'rgba(25, 191, 0, 1)'
+        elif 16 < x['AQI'] <= 33:
+            listForMap[x['index']]['color'] = 'rgba(48, 187, 1, 1)'
+        elif 33 < x['AQI'] <= 50:
+            listForMap[x['index']]['color'] = 'rgba(71, 184, 2, 1)'
+        elif 50 < x['AQI'] <= 66:
+            listForMap[x['index']]['color'] = 'rgba(93, 180, 4, 1)'
+        elif 66 < x['AQI'] <= 83:
+            listForMap[x['index']]['color'] = 'rgba(114, 177, 5, 1)'
+        elif 83 < x['AQI'] <= 100:
+            listForMap[x['index']]['color'] = 'rgba(134, 174, 6, 1)'
+        elif 100 < x['AQI'] <= 116:
+            listForMap[x['index']]['color'] = 'rgba(152, 170, 8, 1)'
+        elif 116 < x['AQI'] <= 133:
+            listForMap[x['index']]['color'] = 'rgba(167, 165, 9, 1)'
+        elif 133 < x['AQI'] <= 150:
+            listForMap[x['index']]['color'] = 'rgba(164, 142, 10, 1)'
+        elif 150 < x['AQI'] <= 166:
+            listForMap[x['index']]['color'] = 'rgba(160, 120, 11, 1)'
+        elif 166 < x['AQI'] <= 183:
+            listForMap[x['index']]['color'] = 'rgba(157, 100, 12, 1)'
+        elif 183 < x['AQI'] <= 200:
+            listForMap[x['index']]['color'] = 'rgba(154, 80, 13, 1)'
+        elif 200 < x['AQI'] <= 233:
+            listForMap[x['index']]['color'] = 'rgba(150, 62, 14, 1)'
+        elif 233 < x['AQI'] <= 266:
+            listForMap[x['index']]['color'] = 'rgba(147, 44, 15, 1)'
+        elif 266 < x['AQI'] <= 300:
+            listForMap[x['index']]['color'] = 'rgba(144, 28, 15, 1)'
+        elif 300 < x['AQI'] <= 366:
+            listForMap[x['index']]['color'] = 'rgba(140, 16, 20, 1)'
+        elif 366 < x['AQI'] <= 433:
+            listForMap[x['index']]['color'] = 'rgba(137, 17, 36, 1)'
+        elif x['AQI'] > 433:
+            listForMap[x['index']]['color'] = 'rgba(134, 18, 51, 1)'
+
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
     app.layout = html.Div(children=[
         dcc.Graph(
@@ -239,7 +294,7 @@ def test(request):
                 'layout': go.Layout(
                     autosize=True,
                     mapbox=dict(
-                        layers = listForMap,
+                        layers=listForMap,
                         accesstoken=mapbox_access_token,
                         bearing=0,
                         center=dict(
